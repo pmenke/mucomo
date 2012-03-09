779f314b364025049c69f34efcb03cc646139eab
Peter Menke <pmenke@googlemail.com>
2012-03-09 09:21:00 +0100

    added stub of RDF interface

---
+++ lib/mucomo/interfaces/rdf.rb
@@ -0,0 +1,21 @@
+module Mucomo
+  module Interfaces
+    module RDF
+      
+    end
+  end
+  
+  module Model
+    
+    class Corpus
+      
+      def as_rdf()
+        ['foo', 'bar', 'bar']
+      end
+      
+    end
+    
+  end
+
+
+end
